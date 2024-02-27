d9ebbe2fc6029b6224241f2e9976f43422ddded6
chenmiaoi <3026066798@qq.com>
2024-02-28 00:29:27 +0800

    feat: create the example frame

---
+++ examples/hello_world.cc
@@ -0,0 +1,6 @@
+#include "execution.hpp"
+
+auto main() -> int {
+
+  return 0;
+}
---
+++ examples/schedulers/static_thread_pool.hpp
@@ -0,0 +1,19 @@
+#ifndef EASY_STDEXEC_STATIC_THREAD_POOL_HPP
+#define EASY_STDEXEC_STATIC_THREAD_POOL_HPP
+
+#include "execution.hpp"
+
+#include <mutex>
+#include <atomic>
+#include <thread>
+#include <condition_variable>
+
+#include <vector>
+#include <exception>
+#include <type_traits>
+
+namespace example::schedulers {
+  // todo pool
+} //! namespace example::schedulers
+
+#endif //EASY_STDEXEC_STATIC_THREAD_POOL_HPP
